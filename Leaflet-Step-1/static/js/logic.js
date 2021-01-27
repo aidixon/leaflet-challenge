3bb81a9238230df0e2f83efd49ad02327568b343
Ashley Dixon <ashleydixon2012@gmail.com>
2021-01-26 18:38:03 -0700

    Updated css file to have mapid and updated leaflet version - map appears

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,18 @@
+// Url https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+
+// Basic map layer
+var myMap = L.map("mapid", {
+    center: [40.58, 105.08],
+    zoom:15
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 510,
+    maxZoom: 20,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
